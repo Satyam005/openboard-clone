97ab0b06c9b9fcf42fa65a3e914cb96335cc4162
Satyam Negi <sn.satyam2014@gmail.com>
2023-07-10 13:03:15 +0530

    Added favicon icon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express(); //Initialize and Server Ready
 
 app.use(express.static("public"));
 
-let port = 3000;
+let port = process.env.PORT || 5000;
 let server = app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
